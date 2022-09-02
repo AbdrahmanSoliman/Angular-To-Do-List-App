b67f1f00d633ad64795542f6d7647ef0ed02c1d3
Abdelrahman A. Soliman <43556094+AbdrahmanSoliman@users.noreply.github.com>
2022-09-02 07:47:43 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { TodoComponent } from './todo/todo.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'', component:TodoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
